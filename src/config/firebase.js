0aef110029db749e0a2b2e0c2e18ab44e7ac2f28
dev <devshukla1245@gmail.com>
2026-07-14 20:52:50 +0530

    Update Firebase Config with production keys

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-// Replace with actual config
 const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDlLP5b5d6yHU9bXUfrc8DQiQocHm42ErY",
+  authDomain: "studyx-8ea20.firebaseapp.com",
+  projectId: "studyx-8ea20",
+  storageBucket: "studyx-8ea20.firebasestorage.app",
+  messagingSenderId: "708689925545",
+  appId: "1:708689925545:web:5e27fc44b7d3540034e0b8",
+  measurementId: "G-ZRCZ7S43Q6"
 };
 
 const app = initializeApp(firebaseConfig);
